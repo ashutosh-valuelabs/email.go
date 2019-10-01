0bc3e2dd30090dd5dbdf29fa4bb564e7f318997d
Ashutosh Prakash <51775050+ashutosh-valuelabs@users.noreply.github.com>
2019-10-01 14:34:43 +0530

    Add files via upload

---
+++ yt_mail.go
@@ -0,0 +1,17 @@
+package main
+import(
+	"gopkg.in/gomail.v2"
+	"fmt"
+)
+func main(){
+	mail:=gomail.NewMessage()
+	mail.SetHeader("From", "email")
+	mail.SetHeader("To", "address")
+	mail.SetHeader("Subject", "/*type your subject */")
+	mail.SetBody("text/plain", "/*Type your message*/")
+	dialer:=gomail.NewPlainDialer("smtp.gmail.com", 587, "/*sender's email address*/", "/*type your password*/")
+	if err:=dialer.DialAndSend(mail);err!=nil{
+		panic(err)
+	}
+	fmt.Println("Email sent")
+}
